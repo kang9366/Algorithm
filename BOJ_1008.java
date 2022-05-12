4062f14938769b9613863bc8a2b2c010da8a2903
Kang SeungGu <63611804+kang9366@users.noreply.github.com>
2022-05-12 19:48:16 +0900

    2022/03/07
    A/B

---
+++ BOJ_1008.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double A = sc.nextDouble(), B = sc.nextDouble();
+        System.out.println((double)(1/3));
+    }
+}
