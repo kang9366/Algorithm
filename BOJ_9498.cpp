8ca3ba41ef9427fae1fe53fe71fb0c6b49454dc5
Kang SeungGu <63611804+kang9366@users.noreply.github.com>
2022-07-14 05:51:22 +0900

    2022/04/02

--- BOJ_9498.cpp
+++ BOJ_9498.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(){
     int score;
     cin >> score;
+ 
     if(score>= 90 && score <= 100) cout << "A" << endl;
     else if(score >= 80 && score <= 89) cout << "B" << endl;
     else if(score >= 70 && score <= 79) cout << "C" << endl;
