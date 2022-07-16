8885a1e1cf4a5354781433937ac8b1bf3c014dc3
Kang SeungGu <63611804+kang9366@users.noreply.github.com>
2022-07-17 03:52:04 +0900

    2022/04/09

---
+++ BOJ_2741.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    int N; cin >> N;
+    for(int i=1; i<=N; i++){
+        cout << i << "\n";
+    }
+}
