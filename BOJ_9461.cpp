6cc3680b06bfaa091bc23f758b4790001670e499
Kang SeungGu <63611804+kang9366@users.noreply.github.com>
2022-07-19 03:35:40 +0900

    Update BOJ_9461.cpp

--- BOJ_9461.cpp
+++ BOJ_9461.cpp
@@ -4,8 +4,27 @@
 using namespace std;
 
 int main(){
-    vector<int> vec(1001, 0);
+    vector<long long> vec;
+    vec.push_back(0);
     vec.push_back(1);
     vec.push_back(1);
-    vec.push_back()
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(2);
+    for(int i = 6; i <= 1000; i++){
+        vec.push_back(vec[i-1] + vec[i-5]);
+    }
+    
+    int num;
+    vector<int> vec2;
+    cin >> num;
+    for(int i = 0; i < num; i++){
+        int temp;
+        cin >> temp;
+        vec2.push_back(temp);
+    }
+    
+    for (int i=0; i<num; i++) {
+        cout << vec[vec2[i]] << endl;
+    }
 }
