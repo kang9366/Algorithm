f7f9212052d91a3f87042ac18b9ca0ee21b36579
Kang SeungGu <63611804+kang9366@users.noreply.github.com>
2022-09-23 00:20:25 +0900

    Add files via upload

---
+++ BOJ_11557.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int T; cin >> T;
+    int temp, num;
+    string college_name;
+    
+    for(int i=0; i<T; i++){
+        cin >> temp;
+        string S[temp]; int L[temp];
+        for(int j=0; j<temp; j++){
+            cin >> college_name >> num;
+            S[j] = college_name; L[j] = num;
+        }
+        cout << S[max_element(L, L+temp) - L] << "\n";
+    }
+}
