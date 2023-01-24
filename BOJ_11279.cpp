ecc9bbb4d64f45f7497d5e883c763f5f95c425ea
Kang SeungGu <63611804+kang9366@users.noreply.github.com>
2023-01-24 18:48:24 +0900

    2023/01/24

---
+++ BOJ_11279.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int N, x;
+    cin >> N;
+    
+    priority_queue<int> pq;
+    
+    for(int i=0; i<N; i++){
+        cin >> x;
+        if(x==0){
+            if(pq.empty()){
+                cout << 0 << "\n";
+            }else{
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        }else{
+            pq.push(x);
+        }
+    }
+}
