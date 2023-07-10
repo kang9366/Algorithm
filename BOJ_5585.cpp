9e83e130eb9200c0e9d4926f248b65e162da9275
SeungGu Kang <alex9366@naver.com>
2023-07-11 00:03:01 +0900

    2023/07/11

---
+++ BOJ_5585.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+#define MONEY 1000
+
+using namespace std;
+
+int main(){
+    int price, change, temp;
+    vector<int> vec = {500, 100, 50, 10, 5, 1};
+    cin >> price;
+    change = MONEY - price;
+
+    int result = 0;
+
+    for(int i=0; i<size(vec); i++){
+        temp = change / vec[i];
+        cout << temp << endl;
+        result += temp;
+        change = change - temp*vec[i];
+    }
+
+    cout << result;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+for(int i=0; i<size(vec); i++){
+    //     cout << "김기류 씨발새끼" << endl;
+    // }
