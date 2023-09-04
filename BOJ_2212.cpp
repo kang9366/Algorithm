f75d108983010b65ff5b3961acca94e8cb6c4dfe
Seunggu Kang <63611804+kang9366@users.noreply.github.com>
2023-09-04 22:52:42 +0900

    2023/09/04

---
+++ BOJ_2212.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int N, K, result=0;
+    cin >> N >> K;
+    int arr[N], diff[N-1];
+    
+
+    for(int i=0; i<N; i++){
+        cin >> arr[i];
+    }
+    sort(arr, arr+N);
+
+    for(int i=0; i<N-1; i++){
+        diff[i] = arr[i+1]-arr[i];
+    }
+    sort(diff, diff+N-1);
+
+    for(int i=0; i<N-K; i++){
+        result += diff[i];
+    }
+
+    cout << result;
+}
