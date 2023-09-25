3caacc81a1874854fbfec068b1e96cd80a41bd3f
Seunggu Kang <63611804+kang9366@users.noreply.github.com>
2023-09-26 01:55:21 +0900

    2023/09/26

--- BOJ_10844.cpp
+++ BOJ_10844.cpp
@@ -1,39 +1,37 @@
 #include <iostream>
-#include <vector>
 
-#define mod 1000000000
 using namespace std;
 
-int dp[101][10] = {
-    0,
-};
-int main()
-{
-    int n;
-    cin >> n;
-
-    for (int i = 1; i < 10; i++) {
-        dp[1][i] = 1;
+int main(){
+    int N;
+    cin >> N;
+    if(N==1){
+        cout << 9;
+        return 0;
     }
+    int dp[N+1][10];
+    dp[1][0] = 0;
+    dp[2][0] = 1;
+    for(int i=1; i<10; i++){
+        dp[1][i] = 1;
+        dp[2][i] = dp[1][i] + dp[1][i-1];
+    }
+    dp[2][9] = 1;
 
-    for (int i = 2; i <= n; i++) {
-        for (int j = 0; j < 10; j++) {
-            if (j == 0)
-                dp[i][0] = dp[i - 1][j + 1];
-            else if (j == 9)
-                dp[i][9] = dp[i - 1][j - 1];
-            else
-                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1];
-
-            dp[i][j] %= mod;
+    for(int i=3; i<=N; i++){
+        for(int j=0; j<10; j++){
+            if(j==0){
+                dp[i][j] = dp[i-1][1]%1000000000;
+            }else if(j==9){
+                dp[i][j] = dp[i-1][8]%1000000000;
+            }else{
+                dp[i][j] = dp[i-1][j-1]%1000000000 + dp[i-1][j+1]%1000000000;
+            }
         }
     }
-
-    int result = 0;
-    for (int i = 0; i < 10; i++) {
-        result = (result + dp[n][i]) % mod;
+    long long result = 0;
+    for(int i=0; i<10; i++){
+        result += dp[N][i];
     }
-    cout << result << "\n";
-
-    return 0;
+    cout << result%1000000000;
 }
